105da3d82ebd7b231a9004dafaa375e63a175273
Diman <diman.1990.63@mail.ru>
2023-03-31 19:24:41 +0300

    Изменения обьектов в базе данных

--- src/main/java/dao/employeeDao/EmployeeDAO.java
+++ src/main/java/dao/employeeDao/EmployeeDAO.java
@@ -1,4 +1,20 @@
 package dao.employeeDao;
 
+import models.Employee;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface EmployeeDAO {
+    Employee findById(Integer id) throws SQLException;
+
+    void addNewEmployee(String firstName, String lastName, String gender, Integer age, Integer cityId)throws SQLException;
+
+
+
+    void refactorEmployee(Integer id, String firstName, String lastName, String gender, Integer age, Integer cityId)throws SQLException;
+
+    void deleteEmployeeById(Integer id)throws SQLException;
+
+    List<Employee> allEmployees() throws SQLException;
 }
